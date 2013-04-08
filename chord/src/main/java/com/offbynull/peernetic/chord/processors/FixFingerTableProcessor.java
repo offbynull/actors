f25d5b5fc96dc86d307fcfa972bfa75ccab58b27
offbynull <offbynull>
2013-04-07 18:34:52 -0700

    Reduced code in FixFingerTableProcessor.java

--- chord/src/main/java/com/offbynull/peernetic/chord/processors/FixFingerTableProcessor.java
+++ chord/src/main/java/com/offbynull/peernetic/chord/processors/FixFingerTableProcessor.java
@@ -1,82 +1,60 @@
 package com.offbynull.peernetic.chord.processors;
 
 import com.offbynull.peernetic.chord.ChordState;
-import com.offbynull.peernetic.eventframework.event.IncomingEvent;
-import com.offbynull.peernetic.eventframework.event.TrackedIdGenerator;
-import com.offbynull.peernetic.eventframework.processor.FinishedProcessResult;
-import com.offbynull.peernetic.eventframework.processor.OngoingProcessResult;
-import com.offbynull.peernetic.eventframework.processor.ProcessResult;
 import com.offbynull.peernetic.eventframework.processor.Processor;
+import com.offbynull.peernetic.eventframework.processor.ProcessorChainAdapter;
+import com.offbynull.peernetic.eventframework.processor.ProcessorException;
 
-public final class FixFingerTableProcessor implements Processor {
+public final class FixFingerTableProcessor
+        extends ProcessorChainAdapter<Boolean> {
 
     private ChordState chordState;
     private int index;
-    private FixFingerProcessor fixFingerProc;
-    private State state;
-
+    
     public FixFingerTableProcessor(ChordState chordState) {
         if (chordState == null) {
             throw new NullPointerException();
         }
-        
-        this.chordState = chordState;
-    }
-    
-    @Override
-    public ProcessResult process(long timestamp, IncomingEvent event,
-            TrackedIdGenerator trackedIdGen) throws Exception {
-        switch (state) {
-            case INIT:
-                return processInitState(timestamp, event, trackedIdGen);
-            case RUNNING:
-            case STALLING:
-                return processRunningState(timestamp, event, trackedIdGen);
-            default:
-                throw new IllegalStateException();
-        }
-    }
 
-    private ProcessResult processInitState(long timestamp, IncomingEvent event,
-            TrackedIdGenerator trackedIdGen) throws Exception {
         if (chordState.getBaseId().getBitCount() == 1) {
             // The finger table only contains the successor, which is updated
             // elsewhere. Do nothing now and force the processor to do nothing
             // for ever.
-            state = State.STALLING;
-            return new OngoingProcessResult();
+            throw new IllegalArgumentException();
         }
         
-        index = 1;
-        fixFingerProc = new FixFingerProcessor(chordState, index);
-        fixFingerProc.process(timestamp, event, trackedIdGen);
-        state = State.RUNNING;
-        return new OngoingProcessResult();
-    }
-
-    private ProcessResult processRunningState(long timestamp,
-            IncomingEvent event, TrackedIdGenerator trackedIdGen)
-            throws Exception {
-        ProcessResult pr = fixFingerProc.process(timestamp, event,
-                trackedIdGen);
+        this.chordState = chordState;
+        this.index = 1;
         
-        if (pr instanceof FinishedProcessResult) {
-            index++;
-            
-            if (index == chordState.getBaseId().getBitCount()) {
-                index = 1;
-            }
-            
-            fixFingerProc = new FixFingerProcessor(chordState, index);
-            fixFingerProc.process(timestamp, event, trackedIdGen);
-        }
-        
-        return new OngoingProcessResult();
+        Processor proc = new FixFingerProcessor(chordState, index);
+        setProcessor(proc);
     }
     
-    private enum State {
-        INIT,
-        RUNNING,
-        STALLING
+    @Override
+    protected NextAction onResult(Processor proc, Object res)
+            throws Exception {
+        index++;
+        
+        if (index == chordState.getBitCount()) {
+            return new ReturnResult(true);
+        }
+        
+        Processor newProc = new FixFingerProcessor(chordState, index);
+        return new GoToNextProcessor(newProc);
+    }
+
+    @Override
+    protected NextAction onException(Processor proc, Exception e)
+            throws Exception {
+        if (e instanceof FixFingerTableFailedException) {
+            throw e;
+        }
+        
+        throw new FixFingerTableFailedException();
+    }
+    
+    public static final class FixFingerTableFailedException
+        extends ProcessorException {
+        
     }
 }

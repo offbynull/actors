f843a71e1b4655b50f2448855b47b425c60b3976
offbynull <offbynull>
2013-11-17 17:09:28 -0800

    - Added common whitelist/blacklist filters

---
+++ p2p-rpc/src/main/java/com/offbynull/p2prpc/common/filters/WhitelistIncomingMessageListener.java
@@ -0,0 +1,50 @@
+package com.offbynull.p2prpc.common.filters;
+
+import com.offbynull.p2prpc.transport.IncomingMessage;
+import com.offbynull.p2prpc.transport.IncomingMessageListener;
+import com.offbynull.p2prpc.transport.IncomingMessageResponseHandler;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import org.apache.commons.lang3.Validate;
+
+public final class WhitelistIncomingMessageListener<A> implements IncomingMessageListener<A> {
+
+    private Set<A> allowedSet;
+
+    public WhitelistIncomingMessageListener(Set<A> disallowedSet) {
+        Validate.noNullElements(disallowedSet);
+
+        this.allowedSet = Collections.newSetFromMap(new ConcurrentHashMap<A, Boolean>());
+        this.allowedSet.addAll(disallowedSet);
+    }
+
+    public void addAddress(A e) {
+        allowedSet.add(e);
+    }
+
+    public void removeAddress(A e) {
+        allowedSet.remove(e);
+    }
+
+    public void addAddresses(Collection<? extends A> c) {
+        allowedSet.addAll(c);
+    }
+
+    public void removeAddresses(Collection<? extends A> c) {
+        allowedSet.removeAll(c);
+    }
+
+    @Override
+    public void messageArrived(IncomingMessage<A> message, IncomingMessageResponseHandler responseCallback) {
+        A from = message.getFrom();
+
+        if (!allowedSet.contains(from)) {
+            throw new AddressNotInWhitelistException();
+        }
+    }
+
+    public static class AddressNotInWhitelistException extends RuntimeException {
+    }
+}

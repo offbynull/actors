1238e46c701352318261b2d45e4969ae6b5ad062
offbynull <offbynull>
2013-11-18 21:07:07 -0800

    - Added new CompositeIncomingMessageListener.java and ModifiableCompositeIncomingMessageListener.java to help bring back functionality that was removed in IncomingMessageListener.java

---
+++ p2p-rpc/src/main/java/com/offbynull/p2prpc/transport/CompositeIncomingMessageListener.java
@@ -0,0 +1,28 @@
+package com.offbynull.p2prpc.transport;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.Validate;
+
+public final class CompositeIncomingMessageListener<A> implements IncomingMessageListener<A> {
+    private List<IncomingMessageListener<A>> listeners;
+
+    public CompositeIncomingMessageListener(List<IncomingMessageListener<A>> listeners) {
+        Validate.noNullElements(listeners);
+        
+        this.listeners = new ArrayList<>(listeners);
+    }
+    
+
+    @Override
+    public void messageArrived(IncomingMessage<A> message, IncomingMessageResponseHandler responseCallback) {
+        try {
+            for (IncomingMessageListener<A> listener : listeners) {
+                listener.messageArrived(message, responseCallback);
+            }
+        } catch (RuntimeException re) {
+            // do nothing
+        }
+    }
+    
+}

b7385f21220ee2cb41ad1862e2c554fd2747d644
offbynull <offbynull>
2013-12-08 00:47:20 -0800

    - Fixes

--- overlay-visualizer/src/main/java/com/offbynull/overlay/visualizer/App.java
+++ overlay-visualizer/src/main/java/com/offbynull/overlay/visualizer/App.java
@@ -6,16 +6,19 @@ public class App {
 
     public static void main(String[] args) {
         VisualizeComponent<Integer> component = new VisualizeComponent<>();
-        NodePlacer<Integer> placer = new RandomLocationNodePlacer<>(1000, 1000);
+
+//        component.addNode(0);
+//        component.removeNode(0);
         
+        VisualizeUtils.displayInWindow("Test", component);
+
         component.addNode(5);
         component.addNode(6);
         component.addNode(1);
-        component.resizeNode(1, 100, 200);
-        component.scaleNode(1, 0.1);
+        component.addNode(7);
+        component.resizeNode(1, 100, 100);
+        component.scaleNode(1, 0.3);
         component.addConnection(5, 6);
         component.setNodeColor(5, Color.RED);
-        
-        VisualizeUtils.displayInWindow("Test", component);
     }
 }
--- overlay-visualizer/src/main/java/com/offbynull/overlay/visualizer/VisualizeComponent.java
+++ overlay-visualizer/src/main/java/com/offbynull/overlay/visualizer/VisualizeComponent.java
@@ -2,6 +2,7 @@ package com.offbynull.overlay.visualizer;
 
 import com.mxgraph.swing.mxGraphComponent;
 import com.mxgraph.util.mxConstants;
+import com.mxgraph.util.mxPoint;
 import com.mxgraph.util.mxRectangle;
 import com.mxgraph.view.mxGraph;
 import com.mxgraph.view.mxGraphView;
@@ -11,10 +12,14 @@ import java.awt.Component;
 import java.awt.LayoutManager;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JComponent;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingUtilities;
 import org.apache.commons.collections4.BidiMap;
 import org.apache.commons.collections4.bidimap.DualHashBidiMap;
 import org.apache.commons.lang3.Validate;
@@ -30,12 +35,12 @@ public final class VisualizeComponent<A> extends JComponent {
     private NodePlacer<A> placer;
 
     public VisualizeComponent() {
-        this(new RandomLocationNodePlacer<A>(1000, 1000));
+        this(new RandomLocationNodePlacer<A>(400, 400));
     }
-    
+
     public VisualizeComponent(NodePlacer<A> placer) {
         Validate.notNull(placer);
-        
+
         graph = new mxGraph();
         graph.setCellsEditable(false);
         graph.setAllowDanglingEdges(false);
@@ -51,24 +56,22 @@ public final class VisualizeComponent<A> extends JComponent {
         graph.setCellsResizable(false);
         graph.setAutoSizeCells(true);
 
-        component = new mxGraphComponent(graph);        
+        component = new mxGraphComponent(graph);
         component.setConnectable(false);
-        
+
         component.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         component.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
-        
 
         super.setLayout(new BorderLayout());
         super.add(component, BorderLayout.CENTER);
-        
+
         nodeLookupMap = new DualHashBidiMap<>();
         connLookupMap = new DualHashBidiMap<>();
         updateLock = new ReentrantLock();
-        
-        this.placer = placer;
-        
-        super.addComponentListener(new ComponentAdapter() {
 
+        this.placer = placer;
+
+        super.addComponentListener(new ComponentAdapter() {
             @Override
             public void componentResized(ComponentEvent e) {
                 zoomFit();
@@ -78,226 +81,272 @@ public final class VisualizeComponent<A> extends JComponent {
 
     public void moveNode(A address, int centerX, int centerY) {
         Validate.notNull(address);
-        
-        updateLock.lock();
+
+        Object vertex = nodeLookupMap.get(address);
+        Validate.isTrue(vertex != null);
+
+        graph.getModel().beginUpdate();
         try {
-            Object vertex = nodeLookupMap.get(address);
-            Validate.isTrue(vertex != null);
-            
-            graph.getModel().beginUpdate();
-            try {
-                mxRectangle rect = graph.getView().getBoundingBox(new Object[] { vertex });
-                graph.moveCells(new Object[] { vertex }, centerX - rect.getCenterX(), centerY - rect.getCenterY());
-            } finally {
-                graph.getModel().endUpdate();
-            }
+            mxRectangle rect = graph.getView().getBoundingBox(new Object[]{vertex});
+            graph.moveCells(new Object[]{vertex}, centerX - rect.getCenterX(), centerY - rect.getCenterY());
         } finally {
-            updateLock.unlock();
+            graph.getModel().endUpdate();
         }
+
+        zoomFit();
     }
 
     public void resizeNode(A address, int width, int height) {
         Validate.notNull(address);
         Validate.inclusiveBetween(0, Integer.MAX_VALUE, width);
         Validate.inclusiveBetween(0, Integer.MAX_VALUE, height);
-        
-        updateLock.lock();
+
+        Object vertex = nodeLookupMap.get(address);
+        Validate.isTrue(vertex != null);
+
+        graph.getModel().beginUpdate();
         try {
-            Object vertex = nodeLookupMap.get(address);
-            Validate.isTrue(vertex != null);
-            
-            graph.getModel().beginUpdate();
-            try {
-                mxRectangle rect = new mxRectangle(0.0, 0.0, width, height);
-                graph.resizeCell(vertex, rect);
-            } finally {
-                graph.getModel().endUpdate();
-            }
+            mxGraphView view = graph.getView();
+            mxRectangle rect = graph.getBoundingBox(vertex);
+
+            rect.setWidth(width);
+            rect.setHeight(height);
+            rect.setX(rect.getX() / view.getScale());
+            rect.setY(rect.getY() / view.getScale());
+            graph.resizeCell(vertex, rect);
         } finally {
-            updateLock.unlock();
+            graph.getModel().endUpdate();
         }
+
+        zoomFit();
     }
 
     public void scaleNode(A address, double scale) {
         Validate.notNull(address);
         Validate.inclusiveBetween(0.0, Double.POSITIVE_INFINITY, scale);
-        
-        updateLock.lock();
+
+        Object vertex = nodeLookupMap.get(address);
+        Validate.isTrue(vertex != null);
+
+        graph.getModel().beginUpdate();
         try {
-            Object vertex = nodeLookupMap.get(address);
-            Validate.isTrue(vertex != null);
-            
-            graph.getModel().beginUpdate();
-            try {
-                mxRectangle rect = graph.getView().getBoundingBox(new Object[] { vertex });
-                
-                rect.setWidth(rect.getWidth() * scale);
-                rect.setHeight(rect.getHeight() * scale);
-                graph.resizeCell(vertex, rect);
-            } finally {
-                graph.getModel().endUpdate();
-            }
+            mxGraphView view = graph.getView();
+            mxRectangle rect = graph.getBoundingBox(vertex);
+
+            rect.setWidth(rect.getWidth() / view.getScale() * scale);
+            rect.setHeight(rect.getHeight() / view.getScale() * scale);
+            rect.setX(rect.getX() / view.getScale());
+            rect.setY(rect.getY() / view.getScale());
+            graph.resizeCell(vertex, rect);
         } finally {
-            updateLock.unlock();
+            graph.getModel().endUpdate();
         }
+
+        zoomFit();
     }
 
     public void setNodeColor(A address, Color color) {
         Validate.notNull(address);
         Validate.notNull(color);
         Validate.isTrue(color.getAlpha() == 255);
-        
-        updateLock.lock();
+
+        Object vertex = nodeLookupMap.get(address);
+        Validate.isTrue(vertex != null);
+
+        graph.getModel().beginUpdate();
         try {
-            Object vertex = nodeLookupMap.get(address);
-            Validate.isTrue(vertex != null);
-            
-            graph.getModel().beginUpdate();
-            try {
-                graph.setCellStyle(mxConstants.STYLE_FILLCOLOR + "=" + "#" + String.format("%06x", color.getRGB() & 0x00FFFFFF),
-                        new Object[] { vertex });
-                nodeLookupMap.put(address, vertex);
-            } finally {
-                graph.getModel().endUpdate();
-            }
+            graph.setCellStyle(mxConstants.STYLE_FILLCOLOR + "=" + "#" + String.format("%06x", color.getRGB() & 0x00FFFFFF),
+                    new Object[]{vertex});
+            nodeLookupMap.put(address, vertex);
         } finally {
-            updateLock.unlock();
+            graph.getModel().endUpdate();
         }
+
+        zoomFit();
     }
-    
+
     public void addNode(A address) {
         Validate.notNull(address);
         Validate.notNull(placer);
-                
+
         NodePlacementInfo info = placer.placeNode(address);
-        
+
         addNode(address, info.getCenterX(), info.getCenterY());
         setNodeColor(address, info.getColor());
         scaleNode(address, info.getScale());
-    }
-    
-    public void addNode(A address, int centerX, int centerY) {
-        Validate.notNull(address);
-        
-        updateLock.lock();
-        try {
-            Validate.isTrue(!nodeLookupMap.containsKey(address));
-            
-            Object parent = graph.getDefaultParent();
-            
-            Object vertex;
-            graph.getModel().beginUpdate();
-            try {
-                vertex = graph.insertVertex(parent, null, address, 0, 0, 1, 1);
-                graph.updateCellSize(vertex);
-                graph.moveCells(new Object[] { vertex }, 0, 0);
-            } finally {
-                graph.getModel().endUpdate();
-            }
 
-            graph.getModel().beginUpdate();
-            try {
-                mxRectangle rect = graph.getView().getBoundingBox(new Object[] { vertex });
-                graph.moveCells(new Object[] { vertex }, centerX - rect.getCenterX(), centerY - rect.getCenterY());
-                nodeLookupMap.put(address, vertex);
-            } finally {
-                graph.getModel().endUpdate();
-            }
-        } finally {
-            updateLock.unlock();
-        }
+        zoomFit();
     }
 
-    public void removeNode(A address) {
-        Validate.notNull(address);
-        
-        updateLock.lock();
+    private void addNode(final A address, final int centerX, final int centerY) {
         try {
-            Validate.isTrue(nodeLookupMap.containsKey(address));
-            
-            graph.getModel().beginUpdate();
-            try {
-                Object vertex = nodeLookupMap.remove(address);
-                Object[] edges = graph.getEdges(vertex);
+            SwingUtilities.invokeAndWait(new Runnable() {
                 
-                for (Object edge : edges) {
-                    connLookupMap.removeValue(edge);
+                @Override
+                public void run() {
+                    
+                    Validate.notNull(address);
+                    Validate.isTrue(!nodeLookupMap.containsKey(address));
+                    
+                    Object parent = graph.getDefaultParent();
+                    
+                    Object vertex;
+                    graph.getModel().beginUpdate();
+                    try {
+                        vertex = graph.insertVertex(parent, null, address, 0, 0, 1, 1);
+                        graph.updateCellSize(vertex);
+                        graph.moveCells(new Object[]{vertex}, 0, 0);
+                    } finally {
+                        graph.getModel().endUpdate();
+                    }
+                    
+                    graph.getModel().beginUpdate();
+                    try {
+                        mxGraphView view = graph.getView();
+                        mxRectangle rect = graph.getBoundingBox(vertex);
+                        
+                        rect.setWidth(rect.getWidth() / view.getScale());
+                        rect.setHeight(rect.getHeight() / view.getScale());
+                        rect.setX(rect.getX() / view.getScale());
+                        rect.setY(rect.getY() / view.getScale());
+                        
+                        graph.moveCells(new Object[]{vertex}, centerX - rect.getCenterX(), centerY - rect.getCenterY());
+                        nodeLookupMap.put(address, vertex);
+                    } finally {
+                        graph.getModel().endUpdate();
+                    }
+                    
+                    zoomFit();
                 }
-                
-                graph.removeCells(new Object[] { vertex });
-            } finally {
-                graph.getModel().endUpdate();
-            }
-        } finally {
-            updateLock.unlock();
+            });
+        } catch (InterruptedException | InvocationTargetException ex) {
         }
     }
-    
-    public void addConnection(A from, A to) {
-        Validate.notNull(from);
-        Validate.notNull(to);
-        
-        updateLock.lock();
+
+    public void removeNode(final A address) {
         try {
-            Object parent = graph.getDefaultParent();
+            Validate.notNull(address);
             
-            graph.getModel().beginUpdate();
-            try {
-                Object fromVertex = nodeLookupMap.get(from);
-                Object toVertex = nodeLookupMap.get(to);
-                ImmutablePair<A, A> key = new ImmutablePair<>(from, to);
-                Validate.isTrue(nodeLookupMap.containsKey(from) && nodeLookupMap.containsKey(to) && !connLookupMap.containsKey(key));
+            SwingUtilities.invokeAndWait(new Runnable() {
                 
-                Object edge = graph.insertEdge(parent, null, null, fromVertex, toVertex);
-                connLookupMap.put(key, edge);
-            } finally {
-                graph.getModel().endUpdate();
-            }
-        } finally {
-            updateLock.unlock();
-        }
-    }
-
-    public void removeConnection(A from, A to) {
-        Validate.notNull(from);
-        Validate.notNull(to);
-
-        updateLock.lock();
-        try {
-            graph.getModel().beginUpdate();
-            try {
-                Object fromVertex = nodeLookupMap.get(from);
-                Object toVertex = nodeLookupMap.get(to);
-                ImmutablePair<A, A> key = new ImmutablePair<>(from, to);
-                Validate.isTrue(nodeLookupMap.containsKey(from) && nodeLookupMap.containsKey(to) && !connLookupMap.containsKey(key));
-                
-                if (fromVertex == null || toVertex == null) {
-                    return;
+                @Override
+                public void run() {
+                    
+                    Validate.isTrue(nodeLookupMap.containsKey(address));
+                    
+                    graph.getModel().beginUpdate();
+                    try {
+                        Object vertex = nodeLookupMap.remove(address);
+                        Object[] edges = graph.getEdges(vertex);
+                        
+                        for (Object edge : edges) {
+                            connLookupMap.removeValue(edge);
+                        }
+                        
+                        graph.removeCells(new Object[]{vertex});
+                    } finally {
+                        graph.getModel().endUpdate();
+                    }
+                    
+                    zoomFit();
                 }
-                
-                Object edge = connLookupMap.get(key);
-                
-                graph.removeCells(new Object[] { edge });
-            } finally {
-                graph.getModel().endUpdate();
-            }
-        } finally {
-            updateLock.unlock();
+            });
+        } catch (InterruptedException | InvocationTargetException ex) {
         }
     }
-    
+
+    public void addConnection(final A from, final A to) {
+        try {
+            Validate.notNull(from);
+            Validate.notNull(to);
+            
+            SwingUtilities.invokeAndWait(new Runnable() {
+                
+                @Override
+                public void run() {
+                    
+                    Object parent = graph.getDefaultParent();
+                    
+                    graph.getModel().beginUpdate();
+                    try {
+                        Object fromVertex = nodeLookupMap.get(from);
+                        Object toVertex = nodeLookupMap.get(to);
+                        ImmutablePair<A, A> key = new ImmutablePair<>(from, to);
+                        Validate.isTrue(nodeLookupMap.containsKey(from) && nodeLookupMap.containsKey(to) && !connLookupMap.containsKey(key));
+                        
+                        Object edge = graph.insertEdge(parent, null, null, fromVertex, toVertex);
+                        connLookupMap.put(key, edge);
+                    } finally {
+                        graph.getModel().endUpdate();
+                    }
+                    
+                    zoomFit();
+                }
+            });
+        } catch (InterruptedException | InvocationTargetException ex) {
+        }
+    }
+
+    public void removeConnection(final A from, final A to) {
+        try {
+            Validate.notNull(from);
+            Validate.notNull(to);
+            
+            SwingUtilities.invokeAndWait(new Runnable() {
+                
+                @Override
+                public void run() {
+                    
+                    graph.getModel().beginUpdate();
+                    try {
+                        Object fromVertex = nodeLookupMap.get(from);
+                        Object toVertex = nodeLookupMap.get(to);
+                        ImmutablePair<A, A> key = new ImmutablePair<>(from, to);
+                        Validate.isTrue(nodeLookupMap.containsKey(from) && nodeLookupMap.containsKey(to) && !connLookupMap.containsKey(key));
+                        
+                        if (fromVertex == null || toVertex == null) {
+                            return;
+                        }
+                        
+                        Object edge = connLookupMap.get(key);
+                        
+                        graph.removeCells(new Object[]{edge});
+                    } finally {
+                        graph.getModel().endUpdate();
+                    }
+                    
+                    zoomFit();
+                }
+            });
+        } catch (InterruptedException | InvocationTargetException ex) {
+        }
+    }
+
     private void zoomFit() {
-        updateLock.lock();
-        try {
-            mxGraphView view = graph.getView();
-            double compLen = Math.min(component.getWidth(), component.getHeight());
-            double viewLen = view.getGraphBounds().getWidth();
-            view.setScale(compLen/viewLen * view.getScale());
-        } finally {
-            updateLock.unlock();
+        mxGraphView view = graph.getView();
+        double compWidth = component.getWidth();
+        double compHeight = component.getHeight();
+        double compLen = Math.min(compWidth, compHeight);
+        mxRectangle graphBounds = view.getGraphBounds();
+        double graphX = (graphBounds.getX()) / view.getScale();
+        double graphY = (graphBounds.getY()) / view.getScale();
+        double graphWidth = (graphBounds.getWidth()) / view.getScale();
+        double graphHeight = (graphBounds.getHeight()) / view.getScale();
+        double graphEndX = graphX + graphWidth;
+        double graphEndY = graphY + graphHeight;
+        
+        double viewLen = Math.max(graphEndX, graphEndY);
+
+        if (Double.isInfinite(viewLen) || Double.isNaN(viewLen) || viewLen <= 0.0) {
+            view.setScale(1.0);
+        } else {
+//                        view.scaleAndTranslate(compLen / viewLen,
+//                    -graphX - (graphWidth - compWidth) / 2,
+//                    -graphY - (graphHeight - compHeight) / 2);
+            view.setScale(compLen / viewLen);
         }
     }
-    
+
     @Override
     public void setLayout(LayoutManager mgr) {
         throw new UnsupportedOperationException();

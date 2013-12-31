b3aeaaeefb2b116a41a71b823fc4443c6e7f55c6
offbynull <offbynull>
2013-12-31 15:13:51 -0800

    - Helper class

---
+++ common/src/main/java/com/offbynull/peernetic/common/nio/utils/ByteBufferUtils.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2013, Kasra Faghihi, All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+package com.offbynull.peernetic.common.nio.utils;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Utility class to help with {@link ByteBuffer}s.
+ * @author Kasra Faghihi
+ */
+public final class ByteBufferUtils {
+    private ByteBufferUtils() {
+        // do nothing
+    }
+
+    /**
+     * Copy the remaining content of a {@link ByteBuffer} in to a new non-direct {@link ByteBuffer}. Equivalent to calling
+     * {@code copyContents(src, true)}.
+     * @param src buffer to copy
+     * @return new buffer with the remaining content in {@code src}
+     * @throws NullPointerException if any arguments are {@code null}
+     */
+    public static ByteBuffer copyContents(ByteBuffer src) {
+        return copyContents(src, true);
+    }
+    
+    /**
+     * Copy the remaining content of a {@link ByteBuffer} in to a new non-direct {@link ByteBuffer}.
+     * @param src buffer to copy
+     * @param incrementSrc of {@code true} increments {@code src}'s position
+     * @return new buffer with the remaining content in {@code src}
+     * @throws NullPointerException if any arguments are {@code null}
+     */
+    public static ByteBuffer copyContents(ByteBuffer src, boolean incrementSrc) {
+        if (!incrementSrc) {
+            src.mark();
+        }
+        
+        ByteBuffer dst = ByteBuffer.allocate(src.remaining());
+        dst.put(src);
+        
+        if (!incrementSrc) {
+            src.reset();
+        }
+        
+        return dst;
+    }
+}

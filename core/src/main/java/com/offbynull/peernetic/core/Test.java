4b333491c432bf91e59028b9b693e366a9ed6378
offbynull <offbynull@gmail.com>
2015-04-09 19:24:17 -0700

    - Removed old debug project - Moved networking (udp) to own project

--- core/src/main/java/com/offbynull/peernetic/core/Test.java
+++ core/src/main/java/com/offbynull/peernetic/core/Test.java
@@ -13,7 +13,6 @@ import com.offbynull.peernetic.core.actors.unreliable.UnreliableProxyCoroutine;
 import com.offbynull.peernetic.core.gateway.Gateway;
 import com.offbynull.peernetic.core.common.SimpleSerializer;
 import com.offbynull.peernetic.core.gateways.timer.TimerGateway;
-import com.offbynull.peernetic.core.gateways.udp.UdpGateway;
 import com.offbynull.peernetic.core.test.TestHarness;
 import java.net.InetSocketAddress;
 import java.time.Duration;
@@ -119,51 +118,51 @@ public class Test {
         latch.await();
     }
 
-    private static void basicUdp() throws InterruptedException {
-        CountDownLatch latch = new CountDownLatch(1);
-
-        Coroutine sender = (cnt) -> {
-            Context ctx = (Context) cnt.getContext();
-            String dstAddr = ctx.getIncomingMessage();
-
-            for (int i = 0; i < 10; i++) {
-                ctx.addOutgoingMessage(dstAddr, i);
-                cnt.suspend();
-                Validate.isTrue(i == (int) ctx.getIncomingMessage());
-            }
-
-            latch.countDown();
-        };
-
-        Coroutine echoer = (cnt) -> {
-            Context ctx = (Context) cnt.getContext();
-
-            while (true) {
-                String src = ctx.getSource();
-                Object msg = ctx.getIncomingMessage();
-                ctx.addOutgoingMessage(src, msg);
-                cnt.suspend();
-            }
-        };
-
-        ActorThread echoerThread = ActorThread.create("echoer");
-        Shuttle echoerInputShuttle = echoerThread.getIncomingShuttle();
-        Gateway echoerUdpGateway = new UdpGateway(new InetSocketAddress(1000), "internaludp", echoerInputShuttle, "echoer:echoer", new SimpleSerializer());
-        Shuttle echoerOutputShuttle = echoerUdpGateway.getIncomingShuttle();
-
-        ActorThread senderThread = ActorThread.create("sender");
-        Shuttle senderInputShuttle = senderThread.getIncomingShuttle();
-        Gateway senderUdpGateway = new UdpGateway(new InetSocketAddress(2000), "internaludp", senderInputShuttle, "sender:sender", new SimpleSerializer());
-        Shuttle senderOutputShuttle = senderUdpGateway.getIncomingShuttle();
-
-        echoerThread.addOutgoingShuttle(echoerOutputShuttle);
-        senderThread.addOutgoingShuttle(senderOutputShuttle);
-
-        echoerThread.addCoroutineActor("echoer", echoer);
-        senderThread.addCoroutineActor("sender", sender, "internaludp:7f000001.1000");
-
-        latch.await();
-    }
+//    private static void basicUdp() throws InterruptedException {
+//        CountDownLatch latch = new CountDownLatch(1);
+//
+//        Coroutine sender = (cnt) -> {
+//            Context ctx = (Context) cnt.getContext();
+//            String dstAddr = ctx.getIncomingMessage();
+//
+//            for (int i = 0; i < 10; i++) {
+//                ctx.addOutgoingMessage(dstAddr, i);
+//                cnt.suspend();
+//                Validate.isTrue(i == (int) ctx.getIncomingMessage());
+//            }
+//
+//            latch.countDown();
+//        };
+//
+//        Coroutine echoer = (cnt) -> {
+//            Context ctx = (Context) cnt.getContext();
+//
+//            while (true) {
+//                String src = ctx.getSource();
+//                Object msg = ctx.getIncomingMessage();
+//                ctx.addOutgoingMessage(src, msg);
+//                cnt.suspend();
+//            }
+//        };
+//
+//        ActorThread echoerThread = ActorThread.create("echoer");
+//        Shuttle echoerInputShuttle = echoerThread.getIncomingShuttle();
+//        Gateway echoerUdpGateway = new UdpGateway(new InetSocketAddress(1000), "internaludp", echoerInputShuttle, "echoer:echoer", new SimpleSerializer());
+//        Shuttle echoerOutputShuttle = echoerUdpGateway.getIncomingShuttle();
+//
+//        ActorThread senderThread = ActorThread.create("sender");
+//        Shuttle senderInputShuttle = senderThread.getIncomingShuttle();
+//        Gateway senderUdpGateway = new UdpGateway(new InetSocketAddress(2000), "internaludp", senderInputShuttle, "sender:sender", new SimpleSerializer());
+//        Shuttle senderOutputShuttle = senderUdpGateway.getIncomingShuttle();
+//
+//        echoerThread.addOutgoingShuttle(echoerOutputShuttle);
+//        senderThread.addOutgoingShuttle(senderOutputShuttle);
+//
+//        echoerThread.addCoroutineActor("echoer", echoer);
+//        senderThread.addCoroutineActor("sender", sender, "internaludp:7f000001.1000");
+//
+//        latch.await();
+//    }
 
     private static void basicTimer() throws InterruptedException {
         CountDownLatch latch = new CountDownLatch(1);

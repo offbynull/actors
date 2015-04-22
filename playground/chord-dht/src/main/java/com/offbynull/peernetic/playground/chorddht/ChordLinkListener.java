8c3ef1ab39c671da9e5995de727beecdc15b6ecc
offbynull <offbynull@gmail.com>
2015-04-22 00:07:59 -0700

    Removed old chorddht and playground

--- playground/chord-dht/src/main/java/com/offbynull/peernetic/playground/chorddht/ChordLinkListener.java
+++
@@ -1,5 +0,0 @@
-package com.offbynull.peernetic.playground.chorddht;
-
-public interface ChordLinkListener<A> {
-    void linked(A id, A dst);
-}
--- playground/chord-dht/src/main/java/com/offbynull/peernetic/playground/chorddht/ChordUnlinkListener.java
+++
@@ -1,5 +0,0 @@
-package com.offbynull.peernetic.playground.chorddht;
-
-public interface ChordUnlinkListener<A> {
-    void unlinked(A id, A dst);
-}

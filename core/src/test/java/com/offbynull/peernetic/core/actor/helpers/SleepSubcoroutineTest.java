ce3761d2a86de02c7c559953b6e35b55385e9212
offbynull <offbynull@gmail.com>
2015-05-03 20:47:59 -0700

    Add SleepSubcoroutine test

---
+++ core/src/test/java/com/offbynull/peernetic/core/actor/helpers/SleepSubcoroutineTest.java
@@ -0,0 +1,31 @@
+package com.offbynull.peernetic.core.actor.helpers;
+
+import com.offbynull.peernetic.core.test.TestHarness;
+import java.time.Duration;
+import java.time.Instant;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SleepSubcoroutineTest {
+    
+    @Test
+    public void mustSleepFor1Second() throws Exception {
+        TestHarness testHarness = new TestHarness("timer");
+        testHarness.addCoroutineActor("test", cnt -> {
+                SleepSubcoroutine sleepSubcoroutine = new SleepSubcoroutine.Builder()
+                        .id("sleep")
+                        .timerAddressPrefix("timer")
+                        .duration(Duration.ofSeconds(1L))
+                        .build();
+                sleepSubcoroutine.run(cnt);
+            }, Duration.ZERO, Instant.ofEpochMilli(0L), "start");
+        
+        Instant time = null;
+        while (testHarness.hasMore()) {
+            time = testHarness.process();
+        }
+        
+        assertEquals(1000L, time.toEpochMilli());
+    }
+    
+}

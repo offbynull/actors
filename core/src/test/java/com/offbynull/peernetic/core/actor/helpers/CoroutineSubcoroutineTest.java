a4f4b101fa307d0c3cad1efcf66f6d9fc4548908
offbynull <offbynull@gmail.com>
2015-05-03 22:07:38 -0700

    Add CoroutineSubcoroutine test

---
+++ core/src/test/java/com/offbynull/peernetic/core/actor/helpers/CoroutineSubcoroutineTest.java
@@ -0,0 +1,29 @@
+package com.offbynull.peernetic.core.actor.helpers;
+
+import com.offbynull.peernetic.core.test.TestHarness;
+import java.time.Duration;
+import java.time.Instant;
+import org.apache.commons.lang3.mutable.MutableBoolean;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class CoroutineSubcoroutineTest {
+
+    @Test
+    public void mustFunnelToCoroutine() {
+        MutableBoolean flag = new MutableBoolean();
+        
+        TestHarness testHarness = new TestHarness("timer");
+        testHarness.addCoroutineActor("test", cnt -> {
+                CoroutineSubcoroutine fixture = new CoroutineSubcoroutine("wrapper", x -> flag.setValue(true));
+                fixture.run(cnt);
+            }, Duration.ZERO, Instant.ofEpochMilli(0L), "start");
+        
+        while (testHarness.hasMore()) {
+            testHarness.process();
+        }
+        
+        assertTrue(flag.booleanValue());
+    }
+    
+}

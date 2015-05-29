bd6b34b3ffa4a553b9269925a6cb8c0f95823a5d
offbynull <offbynull@gmail.com>
2015-05-28 22:26:18 -0700

    Added log gateway that pipes log messages to SLF4J. Not possible to write tests for this, so just manually test when you have time.

---
+++ core/src/main/java/com/offbynull/peernetic/core/gateways/log/LogGateway.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2015, Kasra Faghihi, All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+package com.offbynull.peernetic.core.gateways.log;
+
+import com.offbynull.peernetic.core.actor.Actor;
+import com.offbynull.peernetic.core.gateway.Gateway;
+import com.offbynull.peernetic.core.shuttle.Shuttle;
+import com.offbynull.peernetic.core.gateway.InputGateway;
+import com.offbynull.peernetic.core.shuttles.simple.Bus;
+import com.offbynull.peernetic.core.shuttles.simple.SimpleShuttle;
+import org.apache.commons.lang3.Validate;
+
+/**
+ * {@link Gateway} that logs by piping messages to SLF4J.
+ * <p>
+ * In the following example, the {@link Actor} called {@code tester} sends an info log message to the {@link LogGateway} called
+ * {@code logger}.
+ * <pre>
+ * Coroutine tester = (cnt) -&gt; {
+ *     Context ctx = (Context) cnt.getContext();
+ * 
+ *     String loggerAddress = ctx.getIncomingMessage();
+ *     ctx.addOutgoingMessage(loggerAddress, LogMessage.info("This is an info msg: {} {}", "test arg 1", 2));
+ *     cnt.suspend();
+ * };
+ * 
+ * LogGateway logger = new LogGateway("logger");
+ * Shuttle logInputShuttle = logGateway.getIncomingShuttle();
+ * 
+ * ActorThread testerThread = ActorThread.create("local");
+ * Shuttle testerInputShuttle = testerThread.getIncomingShuttle();
+ * 
+ * testerThread.addOutgoingShuttle(logInputShuttle);
+ * 
+ * testerThread.addCoroutineActor("tester", tester, "logger");
+ * </pre>
+ * @author Kasra Faghihi
+ */
+public final class LogGateway implements InputGateway {
+
+    private final Thread thread;
+    private final Bus bus;
+    
+    private final SimpleShuttle shuttle;
+
+    /**
+     * Constructs a {@link LogGateway} instance.
+     * @param prefix address prefix for this gateway
+     * @throws NullPointerException if any argument is {@code null}
+     */
+    public LogGateway(String prefix) {
+        Validate.notNull(prefix);
+
+        bus = new Bus();
+        shuttle = new SimpleShuttle(prefix, bus);
+        thread = new Thread(new LogRunnable(bus));
+        thread.setDaemon(true);
+        thread.setName(getClass().getSimpleName() + "-" + prefix);
+        thread.start();
+    }
+
+    @Override
+    public Shuttle getIncomingShuttle() {
+        return shuttle;
+    }
+
+    @Override
+    public void close() {
+        thread.interrupt();
+    }
+}

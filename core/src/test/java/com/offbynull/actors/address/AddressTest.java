79710a4062ff853d200b780414f1804d9b7c066c
offbynull <offbynull@gmail.com>
2018-01-01 06:29:41 -0800

    Moved address test to correct area

--- core/src/test/java/com/offbynull/actors/address/AddressTest.java
+++ core/src/test/java/com/offbynull/actors/address/AddressTest.java
@@ -1,6 +1,5 @@
-package com.offbynull.actors.shuttle;
+package com.offbynull.actors.address;
 
-import com.offbynull.actors.address.Address;
 import java.util.Arrays;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
